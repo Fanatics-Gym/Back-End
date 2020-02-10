c74c470c00f49601932a1cc52d93c5a162979b70
jarrodskahill <jarrodskahill@gmail.com>
2020-02-10 01:45:51 -0800

    transfering files

---
+++ backend/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require ('helmet');
+const server = express();
+const userRouter = require('../user/user-router')
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use('/user', userRouter);
+
+
+module.exports = server;
