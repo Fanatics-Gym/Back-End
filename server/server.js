db230639a4e95701e83163b163c10b413de69205
jarrodskahill <jarrodskahill@gmail.com>
2021-01-14 20:02:24 -0600

    testing stripe implementation

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const server = express();
 const userRouter = require("../user/user-router");
 const itemRouter = require("../items/items-router");
+const stripeRouter = require("../stripe/stripePayment");
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use("/user", userRouter);
 server.use("/items", itemRouter);
+server.use("/checkout", stripeRouter);
 
 module.exports = server;
