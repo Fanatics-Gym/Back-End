4e132acc71dd3e6de3060e6cb0f3abaff9c9a720
jarrodskahill <jarrodskahill@gmail.com>
2021-02-20 23:31:28 -0600

    created a route to create application

---
+++ application/application-router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const Appl = require("./application-model");
+
+router.post("/add", (req, res) => {
+  Appl.addApplication(req.body)
+    .then((appl) => {
+      res.status(201).json(appl);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Could not create application" });
+    });
+});
