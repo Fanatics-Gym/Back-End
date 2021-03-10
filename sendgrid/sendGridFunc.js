a8407c722053d1fad2ed006e369042faf31035fb
jarrodskahill <jarrodskahill@gmail.com>
2021-03-09 18:42:11 -0600

    created a file for send grid and a function to send emails

---
+++ sendgrid/sendGridFunc.js
@@ -0,0 +1,16 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const sendMail = (applicant) => {
+  const msg = {
+    to: applicant.email,
+    from: process.env.SENDGRID_EMAIL,
+    subject: "Fanatics Football",
+    text:
+      "Thank you for applying to the Fanatics Football League! We will review your application and get back to you shortly! We have included your information submited as an attachment in a PDF File. ",
+  };
+
+  sgMail.send(msg).catch((err) => {
+    console.log(err);
+  });
+};
