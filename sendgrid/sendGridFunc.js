b877ca5057deff129884a76975aaee81d2e2490c
jarrodskahill <jarrodskahill@gmail.com>
2021-03-09 19:41:10 -0600

    creating an approved email after applicants have been approved for league

--- sendgrid/sendGridFunc.js
+++ sendgrid/sendGridFunc.js
@@ -1,7 +1,12 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-module.exports = (email) => {
+module.exports = {
+  Applied,
+  Approved,
+};
+
+const Applied = (email) => {
   const msg = {
     to: email,
     from: process.env.SENDGRID_EMAIL,
@@ -10,6 +15,19 @@ module.exports = (email) => {
       "Thank you for applying to the Fanatics Football League! We will review your application and get back to you shortly! We have included your information submited as an attachment in a PDF File. ",
   };
 
+  sgMail.send(msg).catch((err) => {
+    console.log(err);
+  });
+};
+
+const Approved = (email) => {
+  const msg = {
+    to: email,
+    from: process.env.SENDGRID_EMAIL,
+    subject: "Fanatics Football",
+    text:
+      "You have been approved for the Fanatics Football League. Follow this link to choose your gear size",
+  };
   sgMail.send(msg).catch((err) => {
     console.log(err);
   });
