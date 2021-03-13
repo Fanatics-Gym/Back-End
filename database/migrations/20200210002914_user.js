4066d5e4c0e7bf1b2a65d50763da868430ac661e
jarrodskahill <jarrodskahill@gmail.com>
2021-03-13 03:20:32 -0600

    updated user info

--- database/migrations/20200210002914_user.js
+++ database/migrations/20200210002914_user.js
@@ -3,10 +3,7 @@ exports.up = function (knex) {
     users.increments();
     users.string("username", 255).notNullable().unique();
     users.string("password", 255).notNullable();
-    users
-      .enu("userType", ["Admin", "Player", "Asistance"])
-      .notNullable()
-      .defaultTo("Player");
+    users.enu("userType", ["Admin", "Player", "Asistance"]).notNullable();
   });
 };
 
