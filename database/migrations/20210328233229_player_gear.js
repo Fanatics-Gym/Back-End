f448ee12dfeceafa7653b4ec00551c3134a2da1a
jarrodskahill <jarrodskahill@gmail.com>
2021-03-28 23:41:13 -0500

    finished creating player gear migration table

--- database/migrations/20210328233229_player_gear.js
+++ database/migrations/20210328233229_player_gear.js
@@ -1,8 +1,20 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("playerGear", (table) => {
+    table.increments("id").unique();
+    table
+      .integer("player_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.enu("helmet", ["M", "L", "XL"]);
+    table.enu("shoulderPads", ["M", "L", "XL"]);
+    table.boolean("backPlate").defaultTo(false);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("playerGear");
 };
