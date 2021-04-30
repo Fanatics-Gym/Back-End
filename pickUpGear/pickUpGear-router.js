772f9bdbd4d0f80cc77e8fe32d9c51cac65eb555
jarrodskahill <jarrodskahill@gmail.com>
2021-04-29 23:08:44 -0500

    date now sent back when added

--- pickUpGear/pickUpGear-router.js
+++ pickUpGear/pickUpGear-router.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 router.post("/addDate", (req, res) => {
   PickUp.addDate(req.body)
     .then((date) => {
-      res.status(200).json(`${req.body} was added as a date`);
+      res.status(200).json(`${req.body.date} was added as a date`);
     })
     .catch((err) => {
       res.status(500).json({ message: "could not add date to pick up" });
