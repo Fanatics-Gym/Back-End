c9b2a9d65c8940fa4e1236904f4e84c138ca846a
jarrodskahill <jarrodskahill@gmail.com>
2021-04-30 00:14:30 -0500

    changed response to date for front end

--- pickUpGear/pickUpGear-router.js
+++ pickUpGear/pickUpGear-router.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 router.post("/addDate", (req, res) => {
   PickUp.addDate(req.body)
     .then((date) => {
-      res.status(200).json(`${req.body.date} was added as a date`);
+      res.status(200).json(date);
     })
     .catch((err) => {
       res.status(500).json({ message: "could not add date to pick up" });
