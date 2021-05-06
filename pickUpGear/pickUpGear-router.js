92b734ab590d156e09e7151152bd907c61013d1a
jarrodskahill <jarrodskahill@gmail.com>
2021-05-05 21:52:09 -0500

    created a new route that will get players and their gear

--- pickUpGear/pickUpGear-router.js
+++ pickUpGear/pickUpGear-router.js
@@ -34,4 +34,15 @@ router.get("/playersPickUp/:id", (req, res) => {
     });
 });
 
+router.get("/playersPickUp", (req, res) => {
+  PickUp.findPlayersWithDate()
+    .then((players) => {
+      res.status(200).json(players);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Could not get players gear" });
+      console.log(err);
+    });
+});
+
 module.exports = router;
