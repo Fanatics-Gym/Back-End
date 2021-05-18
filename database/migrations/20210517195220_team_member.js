3c5a299f23a5d582da6e7cf381176411ea1090af
jarrodskahill <jarrodskahill@gmail.com>
2021-05-17 23:34:03 -0500

    made some changes to team member table

--- database/migrations/20210517195220_team_member.js
+++ database/migrations/20210517195220_team_member.js
@@ -1,15 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable("team_member", (member) => {
-    member.increments("id").unqiue();
+    member.increments("id").unique();
     member
-      .string("team_id")
+      .integer("team_id")
       .references("id")
       .inTable("teams")
       .onDelete("CASCADE")
       .onUpdate("CASCADE")
       .notNullable();
     member
-      .string("user")
+      .integer("user_id")
       .references("id")
       .inTable("users")
       .onDelete("CASCADE")
