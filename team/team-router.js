7d5672c297c8f0b0ca0dc9d20ba289b1dcf3039a
jarrodskahill <jarrodskahill@gmail.com>
2021-05-17 23:28:19 -0500

    created a route to create teams

--- team/team-router.js
+++ team/team-router.js
@@ -22,3 +22,14 @@ router.get("/:id", (req, res) => {
       res.status(500).json({ message: "can't get team" });
     });
 });
+
+router.post("/add", (req, res) => {
+  Team.addTeam(req.body)
+    .then((team) => {
+      res.status(200).json(team);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "couldn't create a team" });
+    });
+});
