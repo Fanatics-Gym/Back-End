503f88134d61fcc77880df29db392f6a4db374f3
jarrodskahill <jarrodskahill@gmail.com>
2021-05-17 20:08:31 -0500

    created a table to connect users with teams

--- database/migrations/20210517195220_team_member.js
+++ database/migrations/20210517195220_team_member.js
@@ -1,3 +1,21 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("team_member", (member) => {
+    member.increments("id").unqiue();
+    member
+      .string("team_id")
+      .references("id")
+      .inTable("teams")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+      .notNullable();
+    member
+      .string("user")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+      .notNullable();
+  });
+};
 
 exports.down = function (knex) {};
