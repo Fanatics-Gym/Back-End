6f874f98e62f4c6c47a956a6ec1067fd871fb099
jarrodskahill <jarrodskahill@gmail.com>
2021-05-17 23:21:34 -0500

    created some routes for teams

---
+++ team/team-router.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Team = require("./team-model");
+
+router.get("/", (req, res) => {
+  Team.allTeams()
+    .then((team) => {
+      res.status(200).json(team);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "failed to get teams" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Team.findTeamById(req.params.id)
+    .then((team) => {
+      res.status(200).json(team);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "can't get team" });
+    });
+});
