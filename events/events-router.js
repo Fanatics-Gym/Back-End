ce587c033e4f54ed573dda31ffa89da3c01ba2ef
jarrodskahill <jarrodskahill@gmail.com>
2021-05-19 21:59:50 -0500

    get event by id

--- events/events-router.js
+++ events/events-router.js
@@ -12,4 +12,15 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  Event.findEventById(req.params.id)
+    .then((event) => {
+      res.status(200).json(event);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "failed to get specific event" });
+    });
+});
+
 module.exports = router;
