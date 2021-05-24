9b9fb0f96edd46829a633ccd8f8518204c4b8eb3
jarrodskahill <jarrodskahill@gmail.com>
2021-05-24 10:56:41 -0500

    added some new events to the events table and added the drop for team events table

--- database/migrations/20210518192823_events.js
+++ database/migrations/20210518192823_events.js
@@ -2,7 +2,14 @@ exports.up = function (knex) {
   return knex.schema.createTable("events", (events) => {
     events.increments("id").unique();
     events
-      .enu("event_type", ["Practice", "Game", "Meeting", "Other"])
+      .enu("event_type", [
+        "Practice",
+        "Game",
+        "Meeting",
+        "Other",
+        "PlayOffs",
+        "Championship",
+      ])
       .notNullable();
     events.string("date").notNullable();
     events.string("time").notNullable();
