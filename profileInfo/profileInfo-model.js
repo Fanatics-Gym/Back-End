12dd1ac4ae2621baa50eb0819ab8fcccf39583dc
jarrodskahill <jarrodskahill@gmail.com>
2021-06-12 20:08:15 -0500

    created some helper functions for profile info

--- profileInfo/profileInfo-model.js
+++ profileInfo/profileInfo-model.js
@@ -2,8 +2,18 @@ const db = require("../database/config");
 
 module.exports = {
   allProfiles,
+  addProfileInfo,
 };
 
 function allProfiles() {
   return db("profileInfo");
+}
+
+function addProfileInfo(user_id, payload) {
+  return db("profileInfo as info")
+    .join("users", function () {
+      this.on("users.id", "=", "info.user_id");
+    })
+    .insert(payload)
+    .where(userId, payload.user_id);
 }
