eac7e79ed8d1dfff70638c948c83363b525492ca
jarrodskahill <jarrodskahill@gmail.com>
2021-06-23 21:35:32 -0500

    created a new route that adds profile info

--- profileInfo/profileInfo-router.js
+++ profileInfo/profileInfo-router.js
@@ -6,7 +6,17 @@ router.get("/", async (req, res) => {
     const profiles = await ProfileInfo.allProfiles();
     res.status(200).json(profiles);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "could not get profiles" });
+  }
+});
+
+router.post("/add", async (req, res) => {
+  try {
+    const profileData = await ProfileInfo.addProfileInfo(req.body);
+    res.status(200).json(profileData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "could not create profile info" });
   }
 });
 
