bf78b41be437c814f54022f0a5e5a538eb974aea
jarrodskahill <jarrodskahill@gmail.com>
2021-06-23 21:26:18 -0500

    created a route to get all profile info

--- profileInfo/profileInfo-router.js
+++ profileInfo/profileInfo-router.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
 const ProfileInfo = require("./profileInfo-model");
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await ProfileInfo.allProfiles();
+    res.status(200).json(profiles);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
